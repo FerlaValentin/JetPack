9897c8272722e425f44efb6f5edcff119c156a9e
MATI2D <senn@esat-alumni.com>
2026-02-19 01:58:12 +0100

    ops....
    el main es importante

--- audio.cc
+++ audio.cc
@@ -3,6 +3,7 @@
 #include <esat/input.h>
 #include <esat/sprite.h>
 #include <esat/time.h>
+#include <time.h>
 #include <esat_extra/soloud/soloud.h>
 #include <esat_extra/soloud/soloud_wav.h>
 #include <stdio.h>
@@ -66,4 +67,49 @@ void AudioInit() {
 
 void FreeAudio(){
   Soloud.deinit();
+}
+const int KWindow_Width = 256 * 2;
+const int KWindow_Height = 192 * 2;
+double delta_time;
+unsigned char fps = 25;
+double current_time;
+double last_time;
+
+
+int esat::main(int argc, char **argv) {
+
+	esat::WindowInit(KWindow_Width, KWindow_Height);
+	esat::WindowSetMouseVisibility(true);
+
+	srand((unsigned)time(nullptr));
+	last_time = esat::Time();
+
+  // Main game loop
+  while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
+    
+    // Calculate time elapsed since the last frame
+    current_time = esat::Time();
+    delta_time = (current_time - last_time) / 1000.0;
+    // Limit delta_time to avoid large jumps
+    if (delta_time > 0.1) delta_time = 0.1;
+    
+    last_time = current_time;
+
+    esat::DrawBegin();
+    esat::DrawClear(0,0,0);
+
+
+
+    // Finish drawing
+    esat::DrawEnd();
+    // Control frame speed (fps)
+    do{
+      current_time = esat::Time();
+    }while((current_time - last_time) <= 1000.0/fps);
+    esat::WindowFrame();
+  }
+
+  // Destroy window
+  esat::WindowDestroy();
+  return 0;
 }
