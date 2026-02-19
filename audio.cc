0e239a1257bce6938029049e550350f38979a5e1
MATI2D <senn@esat-alumni.com>
2026-02-19 01:18:43 +0100

    audio simplificado
    todos los file audio son globales pero no se escuchan si ejecutados en otro acript

--- audio.cc
+++ audio.cc
@@ -3,14 +3,9 @@
 #include <esat/input.h>
 #include <esat/sprite.h>
 #include <esat/time.h>
-
 #include <esat_extra/soloud/soloud.h>
 #include <esat_extra/soloud/soloud_wav.h>
-
 #include <stdio.h>
-
-unsigned char fps=25;
-double current_time,last_time;
 
 enum audio{
   enemyDies,
@@ -19,100 +14,56 @@ enum audio{
   getItem,
   shoot,
   landing,
-  start
-};
-
-enum backgroundAudio{
+  start,
   intro,
   menu,
-  inGame
+  launch
 };
 
-SoLoud::Soloud soloud;    // Engine core
-// necesitaremos mass que solo uno ya que mas de 1 audio en contemporanea
-SoLoud::Wav sonido, back; // Como el spritehandle pero para sonido
+SoLoud::Soloud Soloud;
 
-void PlayAudio(audio selectAudio){
-  // carga un file audio
-  switch(selectAudio){
-    case enemyDies: sonido.load("./assets/audio/enemydies.wav"); printf("0 ");  break;
-    case fallObject:sonido.load("./assets/audio/fallobject.wav");  printf("1 ");break;
-    case getFuel:   sonido.load("./assets/audio/get_fuel.wav");  printf("2 ");  break;
-    case getItem:   sonido.load("./assets/audio/get_item.wav");  printf("3 ");  break;
-    case shoot:     sonido.load("./assets/audio/shoot.wav");  printf("6 ");     break;
-    case landing:   sonido.load("./assets/audio/startgameplay.wav");  printf("8 ");break;
-    case start:     sonido.load("./assets/audio/shoot.wav");  printf("9 ");     break;
+SoLoud::Wav enemyDeath;
+SoLoud::Wav fallSound;
+SoLoud::Wav fuelSound;
+SoLoud::Wav itemSound;
+SoLoud::Wav fireSound;
+SoLoud::Wav landingSound;
+SoLoud::Wav starSound;
+SoLoud::Wav introSound;
+SoLoud::Wav menuSound;
+SoLoud::Wav s_launch;
+
+void PlayAudio(audio selectAudio) {
+  switch(selectAudio) {
+    case enemyDies:   Soloud.play(enemyDeath);    break;
+    case fallObject:  Soloud.play(fallSound);     break;
+    case getFuel:     Soloud.play(fuelSound);     break;
+    case getItem:     Soloud.play(itemSound);     break;
+    case shoot:       Soloud.play(fireSound);     break;
+    case landing:     Soloud.play(landingSound);  break;
+    case start:       Soloud.play(starSound);     break;
+    case intro:       Soloud.play(introSound);    break;
+    case menu:        Soloud.play(menuSound);     break;
+    case launch:      Soloud.play(s_launch);      break;
   }
-  if(sonido.getLength() == 0.0){printf("no load");}
-  printf(" %f ",sonido.getLength());
-  soloud.play(sonido);    // play
+  printf("audio reproducido");
 }
 
-// de esto no estoy seguro
-void PlayBackgroundMusic(backgroundAudio selectAudio){
-  switch (selectAudio)
-  {
-    case intro:
-      back.load("./assets/audio/intro.wav");
-    break;
-    case menu: 
-      back.load("./assets/audio/menu_intro.wav");
-    break;
-    case inGame:
-      back.load("./assets/audio/spacedep1.wav"); 
-    break;
-  }
-  // tengo que repasar la parte del loop
-  back.setLooping(true);
-  soloud.play(back);    // play
+void AudioInit() {
+    Soloud.init();
+
+    enemyDeath.load("./assets/audio/enemydies.wav");
+    fallSound.load("./assets/audio/fallobject.wav");
+    fuelSound.load("./assets/audio/get_fuel.wav");
+    itemSound.load("./assets/audio/get_item.wav");
+    fireSound.load("./assets/audio/shoot.wav");
+    landingSound.load("./assets/audio/startgameplay.wav");
+    starSound.load("./assets/audio/startgameplay.wav");
+    introSound.load("./assets/audio/intro.wav");
+    menuSound.load("./assets/audio/menu_intro.wav");
+    s_launch.load("./assets/audio/spacedep1.wav");
 }
 
-
-int esat::main(int argc, char **argv) {
-  soloud.init();
-  
-	esat::WindowInit(800,600);
-	WindowSetMouseVisibility(true);
-
-  PlayBackgroundMusic(inGame);
-  
-  while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
-    
-    last_time = esat::Time(); 
-    esat::DrawBegin();
-    esat::DrawClear(0,0,0);
-    
-    if(esat::IsSpecialKeyDown(esat::kSpecialKey_Keypad_0)){
-      PlayAudio(enemyDies);
-    }
-    if(esat::IsSpecialKeyDown(esat::kSpecialKey_Keypad_1)){
-      PlayAudio(fallObject);
-    }
-    if(esat::IsSpecialKeyDown(esat::kSpecialKey_Keypad_2)){
-      PlayAudio(getFuel);
-    }
-    if(esat::IsSpecialKeyDown(esat::kSpecialKey_Keypad_3)){
-      PlayAudio(getItem);
-    }
-    if(esat::IsSpecialKeyDown(esat::kSpecialKey_Keypad_6)){
-      PlayAudio(shoot);
-    } 
-    if(esat::IsSpecialKeyDown(esat::kSpecialKey_Keypad_8)){
-      PlayAudio(landing);
-    }    
-    if(esat::IsSpecialKeyDown(esat::kSpecialKey_Keypad_9)){
-      PlayAudio(start);
-    }    
-    
-    esat::DrawEnd();
-    
-    //Control fps 
-    do{
-      current_time = esat::Time();
-    }while((current_time-last_time)<=1000.0/fps);
-    esat::WindowFrame();
-  }
-  soloud.deinit();
-  esat::WindowDestroy();
-  return 0;
+void FreeAudio(){
+  Soloud.deinit();
 }
